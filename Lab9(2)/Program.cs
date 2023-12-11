2b75b7969a46fa959dfe9c39c65b15582c860313
Melnyk Roman <melnikroman2005@ukr.net>
2023-12-11 15:35:34 +0200

    OOP-Lab9 COMMIT

---
+++ Lab9(2)/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Document : IComparable<Document>
+{
+    public string Title { get; set; }
+    public int PageCount { get; set; }
+
+    public int CompareTo(Document other)
+    {
+        return this.PageCount.CompareTo(other.PageCount);
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} ({PageCount} стор.)";
+    }
+}
+
+class DocumentCollection : IEnumerable<Document>
+{
+    internal List<Document> documents = new List<Document>();
+
+    public void AddDocument(Document document)
+    {
+        documents.Add(document);
+    }
+
+    public IEnumerator<Document> GetEnumerator()
+    {
+        documents.Sort();
+        return documents.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DocumentCollection documentCollection = new DocumentCollection();
+
+        while (true)
+        {
+            Console.WriteLine("Введіть інформацію про документ (або 'exit' для завершення):");
+
+            Console.Write("Назва документа: ");
+            string title = Console.ReadLine();
+
+            if (title.ToLower() == "exit")
+                break;
+
+            Console.Write("Кількість сторінок: ");
+            if (!int.TryParse(Console.ReadLine(), out int pageCount))
+            {
+                Console.WriteLine("Некоректне введення кількості сторінок. Спробуйте ще раз.");
+                continue;
+            }
+
+            Document document = new Document { Title = title, PageCount = pageCount };
+            documentCollection.AddDocument(document);
+        }
+
+        Console.WriteLine("\nСписок документів за кількістю сторінок:");
+        foreach (Document document in documentCollection)
+        {
+            Console.WriteLine(document);
+        }
+    }
+}
---
+++ Lab9/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+interface ITrigonometricFigure
+{
+    double GetSurfaceArea();
+}
+
+abstract class TrigonometricFigure : ITrigonometricFigure
+{
+    public abstract double GetSurfaceArea();
+}
+
+class Cylinder : TrigonometricFigure
+{
+    public double Radius { get; set; }
+    public double Height { get; set; }
+
+    public override double GetSurfaceArea()
+    {
+        return 2 * Math.PI * Radius * (Radius + Height);
+    }
+}
+
+class Cube : TrigonometricFigure
+{
+    public double SideLength { get; set; }
+
+    public override double GetSurfaceArea()
+    {
+        return 6 * Math.Pow(SideLength, 2);
+    }
+}
+
+class Cone : TrigonometricFigure
+{
+    public double Radius { get; set; }
+    public double SlantHeight { get; set; }
+
+    public override double GetSurfaceArea()
+    {
+        return Math.PI * Radius * (Radius + SlantHeight);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Виберіть фігуру:");
+            Console.WriteLine("1. Циліндр");
+            Console.WriteLine("2. Куб");
+            Console.WriteLine("3. Конус");
+            Console.WriteLine("0. Вихід");
+
+            int choice;
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Будь ласка, введіть коректне число.");
+                continue;
+            }
+
+            if (choice == 0)
+                break;
+
+            TrigonometricFigure figure = null;
+
+            switch (choice)
+            {
+                case 1:
+                    figure = new Cylinder();
+                    Console.Write("Введіть радіус циліндра: ");
+                    ((Cylinder)figure).Radius = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введіть висоту циліндра: ");
+                    ((Cylinder)figure).Height = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case 2:
+                    figure = new Cube();
+                    Console.Write("Введіть довжину сторони куба: ");
+                    ((Cube)figure).SideLength = Convert.ToDouble(Console.ReadLine());
+                    break;
+                case 3:
+                    figure = new Cone();
+                    Console.Write("Введіть радіус конуса: ");
+                    ((Cone)figure).Radius = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введіть висоту конуса: ");
+                    ((Cone)figure).SlantHeight = Convert.ToDouble(Console.ReadLine());
+                    break;
+                default:
+                    Console.WriteLine("Невідомий вибір. Спробуйте ще раз.");
+                    continue;
+            }
+
+            Console.WriteLine($"Площа поверхні обраної фігури: {figure.GetSurfaceArea()}\n");
+        }
+    }
+}
